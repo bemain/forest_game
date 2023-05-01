dd3915ad5a683e51fa00b0229fec731fcaab1ca9
BeMain <bemain.dev@gmail.com>
2023-05-01 20:42:12 +0200

    Create sample tilemap

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flame/sprite.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +14,47 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: InteractiveViewer(
+          child: GameWidget(
+            game: IsometricTileGame(),
+          ),
         ),
       ),
     );
+  }
+}
+
+class IsometricTileGame extends FlameGame {
+  late IsometricTileMapComponent mapComponent;
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    final tilesetImage = await images.load('tilemaps/sample.png');
+    final tileset = SpriteSheet(
+      image: tilesetImage,
+      srcSize: Vector2.all(64.0),
+    );
+
+    final matrix = [
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+    ];
+
+    mapComponent = IsometricTileMapComponent(
+      tileset,
+      matrix,
+      destTileSize: Vector2.all(32.0),
+      tileHeight: 16.0,
+      position: Vector2.all(200),
+    );
+    await add(mapComponent);
   }
 }
