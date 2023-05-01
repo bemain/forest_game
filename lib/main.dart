2a4eef25ecbd16d412e9b0edf03f7ae9e1ac817e
BeMain <bemain.dev@gmail.com>
2023-05-01 21:02:11 +0200

    Implement selecting a tile by pressing it

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 
 import 'package:flame/components.dart';
+import 'package:flame/extensions.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
 import 'package:flame/sprite.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Image;
 
 void main() {
   runApp(const MainApp());
@@ -26,8 +28,13 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class IsometricTileGame extends FlameGame {
+class IsometricTileGame extends FlameGame with MouseMovementDetector {
   late IsometricTileMapComponent mapComponent;
+  late Selector selector;
+
+  static const double destTileSize = 64.0;
+  static const double tileHeight = destTileSize / 2;
+  final Vector2 topLeft = Vector2.all(200);
 
   @override
   Future<void> onLoad() async {
@@ -40,21 +47,55 @@ class IsometricTileGame extends FlameGame {
     );
 
     final matrix = [
-      [0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0],
+      [2, 2, 2, 2, 2, 2],
+      [2, 2, 2, 2, 2, 2],
+      [2, 2, 2, 2, 2, 2],
+      [2, 2, 2, 2, 2, 2],
+      [2, 2, 2, 2, 2, 2],
+      [2, 2, 2, 2, 2, 2],
     ];
 
     mapComponent = IsometricTileMapComponent(
       tileset,
       matrix,
-      destTileSize: Vector2.all(32.0),
-      tileHeight: 16.0,
-      position: Vector2.all(200),
+      destTileSize: Vector2.all(destTileSize),
+      tileHeight: tileHeight,
+      position: topLeft,
     );
     await add(mapComponent);
+
+    final selectorImage = await images.load('selector.png');
+    selector = Selector(destTileSize, selectorImage);
+    await add(selector);
+  }
+
+  @override
+  void onMouseMove(PointerHoverInfo info) {
+    final Vector2 screenPosition = info.eventPosition.game;
+    final Block block = mapComponent.getBlock(screenPosition);
+
+    selector.show = mapComponent.containsBlock(block);
+    selector.position.setFrom(
+      topLeft + mapComponent.getBlockRenderPosition(block),
+    );
+  }
+}
+
+class Selector extends SpriteComponent {
+  bool show = true;
+
+  Selector(double size, Image image)
+      : super(
+          sprite: Sprite(image, srcSize: Vector2.all(32.0)),
+          size: Vector2.all(size),
+        );
+
+  @override
+  void render(Canvas canvas) {
+    if (!show) {
+      return;
+    }
+
+    super.render(canvas);
   }
 }
